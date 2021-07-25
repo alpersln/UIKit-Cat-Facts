0c790612d141fa572d9ef78bf55f6e79b190ce05
alper <replappdev@gmail.com>
2021-07-25 03:19:46 -0700

    gif added

--- CatFacts/ViewController.swift
+++ CatFacts/ViewController.swift
@@ -11,11 +11,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var catLabel: UILabel!
     var cat: Cat?
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         fetchFacts()
+        
+                let gifURL : String = "https://cataas.com/cat/gif"
+                let imageURL = UIImage.gifImageWithURL(gifURL)
+                let imageView3 = UIImageView(image: imageURL)
+                imageView3.frame = CGRect(x: 20.0, y: 90.0, width: self.view.frame.size.width - 40, height: 150.0)
+                view.addSubview(imageView3)
+        
     }
 
 
@@ -38,6 +46,25 @@ class ViewController: UIViewController {
                 }
             }.resume()
         }
+    }
+    
+    
+    
+    
+    @IBAction func buttonSu(_ sender: Any) {
+        
+        fetchFacts()
+        
+    }
+    
+    
+    @IBAction func gifButton(_ sender: UIButton) {
+        
+        let gifURL : String = "https://cataas.com/cat/gif"
+        let imageURL = UIImage.gifImageWithURL(gifURL)
+        let imageView3 = UIImageView(image: imageURL)
+        imageView3.frame = CGRect(x: 20.0, y: 90.0, width: self.view.frame.size.width - 40, height: 150.0)
+        view.addSubview(imageView3)
     }
 }
 
